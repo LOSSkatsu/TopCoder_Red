0ea44055da298f69cd7190d0b4bb9a700e7670cc
LOSSkatsu <stoicheolwon@gmail.com>
2019-02-15 01:41:47 +0900

    the meaning of return

---
+++ p148_CrazyBot_msol2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+bool grid[100][100] = {false};
+int dx[4] = {1, -1, 0,  0};
+int dy[4] = {0,  0, -1, 1};
+double prob[4]; 
+
+class CrazyBot{
+public:
+	double getProbability(int n, int east, int west, int south, int north){
+		prob[0] = east  / 100.0;
+		prob[1] = west  / 100.0;
+		prob[2] = south / 100.0; 
+		prob[3] = north / 100.0;
+
+		return dfs(50, 50, n) ;
+	}
+
+	double dfs(int x, int y, int n){
+		// 종료 조건 
+		
+		if(grid[x][y]) return 0;
+		if(n < 1) return 1;
+		
+		// 초기 조건 
+		grid[x][y] = true ;
+		double ret = 0;
+
+		for(int i=0; i<4; i++){
+			ret += dfs(x+dx[i], y+dy[i], n-1)*prob[i] ;
+			cout << ret << endl;
+		}
+		grid[x][y] = false;
+		return ret ;  
+	}
+};
+
+
+int main(void){
+	int n = 7;
+	int east = 50;
+	int west = 0;
+	int south = 0;
+	int north = 50;
+
+	CrazyBot CB;
+	double ans = CB.getProbability(n, east, west, south, north);
+
+	cout << ans << endl;
+
+	return 0;
+}
