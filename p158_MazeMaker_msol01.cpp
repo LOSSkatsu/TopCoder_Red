9ccdc6fb91a6c384801a026e4cebb1b8b9eae525
LOSSkatsu <stoicheolwon@gmail.com>
2019-02-16 17:47:30 +0900

    this is very difficult question

---
+++ p158_MazeMaker_msol01.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+
+
+using namespace std;
+
+class MazeMaker{
+public:
+	int longestPath(vector <string> maze, int startRow, int startCol, 
+			vector <int> moveRow, vector <int> moveCol){
+	
+		int max = 0;
+		int width = maze[0].size(); 
+		int height = maze.size();
+		
+		// 판깔기
+		int board[50][50];
+		for(int i=0; i<height; i++){
+			for(int j=0; j<width; j++){
+				board[i][j] = -1;
+			}
+		}
+		board[startRow][startCol] = 0;
+
+		// 큐 생성 및 스타트지점 설정
+		queue <int> queueX;
+		queue <int> queueY;
+		queueX.push(startCol);
+		queueY.push(startRow);
+
+		while( queueX.size() > 0 ){
+			int x = queueX.front();
+			int y = queueY.front();
+
+			queueX.pop();
+			queueY.pop();
+			
+			for(int i=0; i<moveRow.size(); i++){
+				int nextX = x + moveCol[i]; 
+				int nextY = y + moveRow[i];
+
+				if(0<= nextX && nextX < width
+				&& 0<= nextY && nextY < height
+				&& board[nextY][nextX] == -1
+				&& maze[nextY].substr(nextX, 1) == "."){
+					
+					board[nextY][nextX] = board[y][x] + 1;
+					queueX.push(nextX); 
+					queueY.push(nextY);
+				
+				}
+			}
+		}
+
+		for(int i=0; i<height; i++){
+			for(int j=0; j<width; j++){
+				if(maze[i].substr(j,1) == "." && board[i][j] == -1)
+					return -1;
+				max = std::max(max, board[i][j]); 
+			}
+		}
+		
+		return max;
+	}
+};
+
+int main(void){
+
+	vector <string> maze = {"......."};
+	int startRow = 0;
+	int startCol = 0;
+
+	vector <int> moveRow = {1,0,1,0,1,0};
+	vector <int> moveCol = {0,1,0,1,0,1};
+
+	MazeMaker MM; 
+	int result = MM.longestPath(maze, startRow, startCol, moveRow, moveCol); 
+
+	cout << result << endl;
+
+	return 0;
+}
